7f2ebf110c6d4fc099ba42ab946c1aaf04a86758
JishenWang <1801076890@qq.com>
2026-04-24 20:37:40 +0800

    1

--- pet-hospital/src/main/java/com/pethospital/pet_hospital/filter/JwtAuthenticationFilter.java
+++ pet-hospital/src/main/java/com/pethospital/pet_hospital/filter/JwtAuthenticationFilter.java
@@ -1,10 +1,7 @@
 package com.pethospital.pet_hospital.filter;
 
-import com.pethospital.pet_hospital.utils.JwtUtil;
-import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -15,7 +12,12 @@ import org.springframework.security.web.authentication.WebAuthenticationDetailsS
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-import java.io.IOException;
+import com.pethospital.pet_hospital.utils.JwtUtil;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
